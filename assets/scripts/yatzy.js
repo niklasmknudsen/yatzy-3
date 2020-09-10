1c930c864b3c5da190987db9b17583f3b3a709aa
nikla <nk@komunikado.dk>
2020-09-11 00:06:50 +0200

    [added] methods for getDieValues()

--- assets/scripts/yatzy.js
+++ assets/scripts/yatzy.js
@@ -1,4 +1,4 @@
-const playboardcontainer = document.getElementsByClassName('yatzy-playboard__container')[0]; // grabs playboard
+let playboardcontainer = document.getElementsByClassName('yatzy-playboard__container')[0]; // grabs playboard
 
 const gameOptions = [
     "1-s",
@@ -6,7 +6,7 @@ const gameOptions = [
     "3-s",
     "4-s",
     "5-s",
-    "6-s", "sum", "bonus",
+    "6-s",
     "One pair", 
     "Two pairs",
     "three same",
@@ -14,8 +14,10 @@ const gameOptions = [
     "full house",
     "small straight",
     "large straight",
-    "chance", "yatzy", "total"
+    "chance", "yatzy"
 ];
+
+const gameSumOptions = ["sum", "bonus", "total"];
 
 
 function initialiseYatzyBoard() {
@@ -24,13 +26,24 @@ function initialiseYatzyBoard() {
 }
 
 function buildPlayBoard() {
+    if (playboardcontainer) {
+        for (let i = 0; i < gameOptions.length; i++) {
+            let gridRow = createElement('div');
+            gridRow.classList.add('grid-row');
+            gridRow.classList.add('grid-row__left');
+            gridRow.innerHTML = `<label> ${gameOptions[i]} </label>
+            <input class="yatzy-playboard__inputfields" type="text" disabled />`;
+            playboardcontainer.appendChild(gridRow);
+        }
 
-    for (let i = 0; i < gameOptions.length; i++) {
-        let gridRow = createElement('div');
-        gridRow.classList.add('grid-row');
-        gridRow.innerHTML = `<label> ${gameOptions[i]} </label>
-        <input class="yatzy-playboard__inputfields" type="text" disabled />`;
-        playboardcontainer.appendChild(gridRow);
+        for (let j = 0; j < gameSumOptions.length; j++) {
+            let gridRow = createElement('div');
+            gridRow.classList.add('grid-row');
+            gridRow.classList.add('grid-row__right');
+            gridRow.innerHTML = `<label class="yatzy-playboard_label"> ${gameSumOptions[j]} </label>
+            <input class="yatzy-playboard__inputfields" type="text" disabled />`;
+            playboardcontainer.appendChild(gridRow);
+        }
     }
 }
 
@@ -41,6 +54,7 @@ function getDieValues() {
     let resultSet = frequency(dice);
     console.log(resultSet);
 }
+
 
 function frequency(array) {
     let freq = [];
